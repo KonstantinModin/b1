0df5e8c20dad4f44a7a12219b5325a6af943f0ad
KonstantinModin <konstantin.v.modin@gmail.com>
2020-01-09 14:03:41 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,193 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import first from './woman-camera.jpg';
+import second from './spiderweb.jpg';
+import third from './hearthand.jpg';
+import { Carousel, Navbar, Nav, Form, FormControl, Button, Card, CardColumns } from 'react-bootstrap';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section id="cover">
+        <div id="cover-caption">
+          <div className="container">
+            <div className="col-sm-10 col-sm-offset-1"></div>
+            <h1 className="display-3">Welcome to Bootstrap 4</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              possimus veritatis maiores soluta ut modi illum expedita at
+              impedit voluptatem accusamus officiis asperiores cum aut vitae
+              vero iure. Quasi, quos?
+            </p>
+            <form action="" className="form-inline">
+              <div className="form-group">
+                <label className="sr-only">Name</label>
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="John Malcovich"
+                ></input>
+                <label className="sr-only">E-mail</label>
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="john.malcovich@gmail.com"
+                ></input>
+              </div>
+              <button className="btn btn-success btn-lg">Get it!</button>
+            </form>
+            <br />
+            <button className="btn btn-secondary-outline btn-sm">&darr;</button>
+          </div>
+        </div>
+      </section>
+
+      {/* NavBar */}
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+
+      {/* Carousel */}
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={first}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={second}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={third}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+          </Carousel.Item>
+      </Carousel>
+
+      <CardColumns>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title that wraps to a new line</Card.Title>
+      <Card.Text>
+        This is a longer card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <Card className="p-3">
+    <blockquote className="blockquote mb-0 card-body">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+        erat a ante.
+      </p>
+      <footer className="blockquote-footer">
+        <small className="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card bg="primary" text="white" className="text-center p-3">
+    <blockquote className="blockquote mb-0 card-body">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+        erat a ante.
+      </p>
+      <footer className="blockquote-footer">
+        <small className="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </Card>
+  <Card className="text-center">
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+      <Card.Text>
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <Card>
+    <Card.Img src="holder.js/100px160" />
+  </Card>
+  <Card className="text-right">
+    <blockquote className="blockquote mb-0 card-body">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+        erat a ante.
+      </p>
+      <footer className="blockquote-footer">
+        <small className="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </Card>
+  <Card>
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+      <Card.Text>
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+</CardColumns>      
+    </>
   );
 }
 
