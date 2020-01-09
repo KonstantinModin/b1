189e675bef95576db6b712ab111450b312541d51
KonstantinModin <konstantin.v.modin@gmail.com>
2020-01-09 15:09:29 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
-import first from './woman-camera.jpg';
-import second from './spiderweb.jpg';
-import third from './hearthand.jpg';
-import { Carousel, Navbar, Nav, Form, FormControl, Button, Card, CardColumns } from 'react-bootstrap';
+import first from "./woman-camera.jpg";
+import second from "./spiderweb.jpg";
+import third from "./hearthand.jpg";
+import prog from './programming.jpg';
+import chalk from './chalkboard.jpg';
+import type from './working.jpg';
+import {
+  Carousel,
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  Card,
+  CardColumns
+} from "react-bootstrap";
 import "./App.css";
 
 function App() {
@@ -61,132 +73,123 @@ function App() {
       {/* Carousel */}
       <Carousel>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={first}
-            alt="First slide"
-          />
+          <img className="d-block w-100" src={first} alt="First slide" />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={second}
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={second} alt="Third slide" />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={third}
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={third} alt="Third slide" />
           <Carousel.Caption>
             <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
           </Carousel.Caption>
-          </Carousel.Item>
+        </Carousel.Item>
       </Carousel>
 
-      <CardColumns>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title that wraps to a new line</Card.Title>
-      <Card.Text>
-        This is a longer card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card className="p-3">
-    <blockquote className="blockquote mb-0 card-body">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>
-      <footer className="blockquote-footer">
-        <small className="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
-        </small>
-      </footer>
-    </blockquote>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card bg="primary" text="white" className="text-center p-3">
-    <blockquote className="blockquote mb-0 card-body">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>
-      <footer className="blockquote-footer">
-        <small className="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
-        </small>
-      </footer>
-    </blockquote>
-  </Card>
-  <Card className="text-center">
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Img src="holder.js/100px160" />
-  </Card>
-  <Card className="text-right">
-    <blockquote className="blockquote mb-0 card-body">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>
-      <footer className="blockquote-footer">
-        <small className="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
-        </small>
-      </footer>
-    </blockquote>
-  </Card>
-  <Card>
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-</CardColumns>      
+      <CardColumns className="cards">
+        <Card>
+          <Card.Img variant="top" src={prog} />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="p-3">
+          <blockquote className="blockquote mb-0 card-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante.
+            </p>
+            <footer className="blockquote-footer">
+              <small className="text-muted">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </small>
+            </footer>
+          </blockquote>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={chalk} />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card bg="primary" text="white" className="text-center p-3">
+          <blockquote className="blockquote mb-0 card-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante.
+            </p>
+            <footer className="blockquote-footer">
+              <small className="text-muted">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </small>
+            </footer>
+          </blockquote>
+        </Card>
+        <Card className="text-center">
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+            <Card.Text>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img src={type} />
+        </Card>
+        <Card className="text-right">
+          <blockquote className="blockquote mb-0 card-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante.
+            </p>
+            <footer className="blockquote-footer">
+              <small className="text-muted">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </small>
+            </footer>
+          </blockquote>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+            <Card.Text>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardColumns>
     </>
   );
 }
