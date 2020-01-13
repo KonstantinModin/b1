960d77e8c51c7cd99c7e65ebcac73673024db342
KonstantinModin <konstantin.v.modin@gmail.com>
2020-01-13 06:11:48 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -6,98 +6,23 @@ import prog from "./programming.jpg";
 import chalk from "./chalkboard.jpg";
 import type from "./working.jpg";
 import stars from './stars.jpg';
-import {
-  Carousel,
-  Navbar,
-  Nav,
-  Form,
-  FormControl,
+import {  
   Button,
   Card,
   CardColumns,
   CardGroup
 } from "react-bootstrap";
+import Cover from './Cover';
+import Navbar from './NavBar';
+import Carousel from './Carousel';
 import "./App.css";
 
 function App() {
   return (
     <>
-      <section id="cover">
-        <div id="cover-caption">
-          <div className="container">
-            <div className="col-sm-10 offset-sm-1"></div>
-            <h1 className="display-3">Welcome to Bootstrap 4</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-              possimus veritatis maiores soluta ut modi illum expedita at
-              impedit voluptatem accusamus officiis asperiores cum aut vitae
-              vero iure. Quasi, quos?
-            </p>
-            <form action="" className="form-inline justify-content-center">
-              <div className="form-group">
-                <label className="sr-only">Name</label>
-                <input
-                  type="text"
-                  className="form-control form-control-lg"
-                  placeholder="John Malcovich"
-                ></input>
-                <label className="sr-only">E-mail</label>
-              </div>
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control form-control-lg"
-                  placeholder="john.malcovich@gmail.com"
-                ></input>
-              </div>
-              <button className="btn btn-success btn-lg">Get it!</button>
-            </form>
-            <br />
-            <button className="btn btn-outline-light btn-sm">&darr;</button>
-          </div>
-        </div>
-      </section>
-
-      {/* NavBar */}
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
-
-      {/* Carousel */}
-      <Carousel>
-        <Carousel.Item>
-          <img className="d-block w-100" src={first} alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={second} alt="Third slide" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={third} alt="Third slide" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <Cover />
+      <Navbar />      
+      <Carousel />
 
       <CardColumns className="cards">
         <Card>
