345827458c408c8481c5c632ba61411f7e7db599
KonstantinModin <konstantin.v.modin@gmail.com>
2020-01-13 06:11:55 +0100

    updates

---
+++ src/Carousel.js
@@ -0,0 +1,37 @@
+import React from "react";
+import first from "./woman-camera.jpg";
+import second from "./spiderweb.jpg";
+import third from "./hearthand.jpg";
+import { Carousel } from 'react-bootstrap';
+
+const CarouselMain = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src={first} alt="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={second} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={third} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default CarouselMain;
---
+++ src/Cover.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Cover = () => {
+    return (
+        <section id="cover">
+        <div id="cover-caption">
+          <div className="container">
+            <div className="col-sm-10 offset-sm-1"></div>
+            <h1 className="display-3">Welcome to Bootstrap 4</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              possimus veritatis maiores soluta ut modi illum expedita at
+              impedit voluptatem accusamus officiis asperiores cum aut vitae
+              vero iure. Quasi, quos?
+            </p>
+            <form action="" className="form-inline justify-content-center">
+              <div className="form-group">
+                <label className="sr-only">Name</label>
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="John Malcovich"
+                ></input>
+                <label className="sr-only">E-mail</label>
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="john.malcovich@gmail.com"
+                ></input>
+              </div>
+              <button className="btn btn-success btn-lg">Get it!</button>
+            </form>
+            <br />
+            <button className="btn btn-outline-light btn-sm">&darr;</button>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default Cover;
