0cdfa416dbecb675c2372b2e5b75b32f2e2805a6
KonstantinModin <konstantin.v.modin@gmail.com>
2020-01-14 09:37:42 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { StickyContainer, Sticky } from 'react-sticky';
+import React, { useEffect, useState } from "react";
+// import { StickyContainer, Sticky } from 'react-sticky';
+import { Container } from 'react-bootstrap';
 
 import Cover from "./Cover";
 import Navbar from "./NavBar";
@@ -12,8 +13,27 @@ import Footer from "./Footer";
 import "./App.css";
 
 const App = () => {
+    // const ref = React.createRef();
+    // const [sticky, setSticky] = useState(false);
+
+    // useEffect(()=>{
+    //     const scrollHandler = () => {
+    //         setSticky(ref.current ? (window.scrollY >= ref.current.offsetTop) : false);
+    //         // console.log(window.scrollY, ref.current.offsetTop, sticky, ref.current, window.scrollY >= ref.current.offsetTop );
+    //     }
+
+    //     window.addEventListener('scroll', scrollHandler);
+    //     return () => window.removeEventListener('scroll', scrollHandler);
+    // },[]);
+
+    
+
     return (
-        <>
+        <Container 
+            className="firstDiv"
+            // onScroll={}
+        >
+            {/* <div className="system">{sticky.toString()}</div> */}
            <Cover />
             {/* <StickyContainer> */}
             {/* <Sticky> */}
@@ -26,7 +46,7 @@ const App = () => {
             <News />
             <Footer />
             {/* </StickyContainer> */}
-        </>
+        </Container>
     );
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,21 +2,19 @@ import React, { useRef, useEffect, useState } from "react";
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 const NavBar = () => {
-  const nav = useRef();
+  // const nav = useRef();
   // const [offset, setOfset ] = useState(null);
 
-  useEffect(()=>{
-    const sticky = window.scrollY >= nav.current.offsetTop;
-    console.log(sticky, window.scrollY);
-  },[window.scrollY]);
+  // useEffect(()=>{
+  //   // const sticky = window.scrollY >= nav.current.offsetTop;
+  //   console.log(props.sticky);
+  // },[props.sticky]);
 
-  console.log(window);
+  // console.log(window);
 
 
   return (
-    <Navbar bg="dark" variant="dark" ref={nav} >
-      {/* {nav.current&&nav.current.offsetTop} */}
-      {/* {sticky.toString()} */}
+    <Navbar sticky="top" bg="dark" variant="dark" >     
       <Navbar.Brand href="#home">Navbar</Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
